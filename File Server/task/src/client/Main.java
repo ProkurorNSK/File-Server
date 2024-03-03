ffb6392815f97f01f3537eec9c683c5a9c8ad200
ProkurorNSK <fedyashin.pavel@yandex.ru>
2024-03-04 02:09:12 +0700

    Finish

--- File Server/task/src/client/Main.java
+++ File Server/task/src/client/Main.java
@@ -2,15 +2,23 @@ package client;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
-import java.io.IOException;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
     private static final String SERVER_ADDRESS = "127.0.0.1";
-    private static final int SERVER_PORT = 34522;
+    private static final int SERVER_PORT = 34523;
+    private static final String partOfPath = System.getProperty("user.dir") + "\\src\\client\\data\\";
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args){
+        try {
+            TimeUnit.MILLISECONDS.sleep(300);
+        } catch (InterruptedException ignored) {
+        }
+
         try (
                 Socket socket = new Socket(SERVER_ADDRESS, SERVER_PORT);
                 DataInputStream input = new DataInputStream(socket.getInputStream());
@@ -18,48 +26,91 @@ public class Main {
         ) {
             Scanner sc = new Scanner(System.in);
             System.out.print("Enter action (1 - get a file, 2 - create a file, 3 - delete a file): ");
-            String command = sc.next();
+            String command = sc.nextLine();
             switch (command) {
                 case "1" -> {
-                    System.out.print("Enter filename: ");
-                    String name = sc.next();
-                    output.writeUTF("GET " + name);
+                    System.out.print("Do you want to get the file by name or by id (1 - name, 2 - id): ");
+                    String type = sc.nextLine();
+                    output.writeInt(1);
+                    output.writeInt(Integer.parseInt(type));
+                    switch (type) {
+                        case "1" -> {
+                            System.out.print("Enter filename: ");
+                            String name = sc.nextLine();
+                            output.writeUTF(name);
+                        }
+                        case "2" -> {
+                            System.out.print("Enter id: ");
+                            String id = sc.nextLine();
+                            output.writeInt(Integer.parseInt(id));
+                        }
+                    }
                     System.out.println("The request was sent.");
-                    String receivedMsg = input.readUTF();
-                    switch (receivedMsg.substring(0, 3)) {
-                        case "200" -> System.out.println("The content of the file is: " + receivedMsg.substring(4));
-                        case "404" -> System.out.println("The response says that the file was not found!");
+                    int status = input.readInt();
+                    switch (status) {
+                        case 200 -> {
+                            int dataLength = input.readInt();
+                            byte[] data = new byte[dataLength];
+                            //noinspection ResultOfMethodCallIgnored
+                            input.read(data, 0, dataLength);
+                            System.out.print("The file was downloaded! Specify a name for it: ");
+                            String name = sc.nextLine();
+                            Files.write(Paths.get(partOfPath + name), data);
+                            System.out.println("File saved on the hard drive!");
+                        }
+                        case 404 -> System.out.println("The response says that this file is not found!");
                     }
                 }
                 case "2" -> {
-                    System.out.print("Enter filename: ");
-                    String name = sc.next();
-                    System.out.print("Enter file content: ");
-                    String content = sc.next();
-                    output.writeUTF("PUT " + name + " " + content);
+                    System.out.print("Enter name of the file: ");
+                    String name = sc.nextLine();
+                    System.out.print("Enter name of the file to be saved on server: ");
+                    String nameServer = sc.nextLine();
+                    byte[] nameServerBytes = nameServer.getBytes();
+                    byte[] message = Files.readAllBytes(Paths.get(partOfPath + name));
+                    output.writeInt(2);
+                    output.writeInt(nameServerBytes.length);
+                    output.write(nameServerBytes);
+                    output.writeInt(message.length);
+                    output.write(message);
                     System.out.println("The request was sent.");
-                    String receivedMsg = input.readUTF();
-                    switch (receivedMsg) {
-                        case "200" -> System.out.println("The response says that the file was created!");
-                        case "403" -> System.out.println("The response says that creating the file was forbidden!");
+                    int status = input.readInt();
+                    switch (status) {
+                        case 200 -> System.out.println("Response says that file is saved! ID = " + input.readInt());
+                        case 403 -> System.out.println("The response says that creating the file was forbidden!");
                     }
                 }
                 case "3" -> {
-                    System.out.print("Enter filename: ");
-                    String name = sc.next();
-                    output.writeUTF("DELETE " + name);
+                    System.out.print("Do you want to delete the file by name or by id (1 - name, 2 - id): ");
+                    String type = sc.nextLine();
+                    output.writeInt(3);
+                    output.writeInt(Integer.parseInt(type));
+                    switch (type) {
+                        case "1" -> {
+                            System.out.print("Enter filename: ");
+                            String name = sc.nextLine();
+                            output.writeUTF(name);
+                        }
+                        case "2" -> {
+                            System.out.print("Enter id: ");
+                            String id = sc.nextLine();
+                            output.writeInt(Integer.parseInt(id));
+                        }
+                    }
                     System.out.println("The request was sent.");
-                    String receivedMsg = input.readUTF();
-                    switch (receivedMsg) {
-                        case "200" -> System.out.println("The response says that the file was successfully deleted!");
-                        case "404" -> System.out.println("The response says that the file was not found!");
+                    int status = input.readInt();
+                    switch (status) {
+                        case 200 -> System.out.println("The response says that this file was deleted successfully!");
+                        case 404 -> System.out.println("The response says that this file is not found!");
                     }
                 }
                 case "exit" -> {
-                    output.writeUTF(command);
+                    output.writeInt(0);
                     System.out.println("The request was sent.");
+                    input.readInt();
                 }
             }
+        } catch (Exception ignored) {
         }
     }
 }
--- File Server/task/src/server/Main.java
+++ File Server/task/src/server/Main.java
@@ -1,67 +1,138 @@
 package server;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.file.*;
+import java.util.concurrent.*;
 
 import static java.nio.file.StandardOpenOption.*;
 
 public class Main {
-
-    //    private static final Map<String, Boolean> files = new HashMap<>();
-    private static final int PORT = 34522;
+    private static final int PORT = 34523;
     private static final String partOfPath = System.getProperty("user.dir") + "\\src\\server\\data\\";
+    private static final String pathMap = System.getProperty("user.dir") + "\\src\\server\\metadata\\map.txt";
+    private static final ExecutorService executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+    private static ConcurrentMap<Integer, String> map;
+    private static int id;
+    private static ServerSocket serverSocket;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
+        try (ObjectInputStream ois = new ObjectInputStream(new BufferedInputStream(new FileInputStream(pathMap)))) {
+            //noinspection unchecked
+            map = (ConcurrentHashMap<Integer, String>) ois.readObject();
+            id = map.keySet().stream().mapToInt(Integer::intValue).max().orElse(0);
+        } catch (ClassNotFoundException | IOException exception) {
+            map = new ConcurrentHashMap<>();
+            id = 0;
+        }
+
         try (ServerSocket server = new ServerSocket(PORT)) {
-//            System.out.println("Server started!");
+            serverSocket = server;
+            //noinspection InfiniteLoopStatement
             while (true) {
-                try (
-                        Socket socket = server.accept();
-                        DataInputStream input = new DataInputStream(socket.getInputStream());
-                        DataOutputStream output = new DataOutputStream(socket.getOutputStream())
-                ) {
-                    String[] msg = input.readUTF().split(" ");
+                Socket socket = server.accept();
+                Session session = new Session(socket);
+                executor.submit(session);
+            }
+        } catch (IOException ignored) {
+        } finally {
+            executor.shutdown();
+        }
+    }
 
-                    switch (msg[0]) {
-                        case "PUT" -> {
-                            String name = msg[1];
-                            String content = msg[2];
-                            Path path = Paths.get(partOfPath + name);
-                            try {
-                                Files.writeString(path, content, CREATE_NEW, WRITE);
-                                output.writeUTF("200");
-                            } catch (IOException e) {
-                                output.writeUTF("403");
-                            }
+    private static synchronized int getNextID() {
+        id++;
+        return id;
+    }
+
+    private static void stopServer() {
+        executor.shutdown();
+        try (ObjectOutputStream ous = new ObjectOutputStream(new BufferedOutputStream(new FileOutputStream(pathMap)))) {
+            serverSocket.close();
+            ous.writeObject(map);
+        } catch (IOException ignored) {
+        }
+        System.exit(0);
+    }
+
+    static class Session implements Runnable {
+        private final Socket socket;
+
+        public Session(Socket socket) {
+            this.socket = socket;
+        }
+
+        @Override
+        public void run() {
+            try (DataInputStream input = new DataInputStream(socket.getInputStream()); DataOutputStream output = new DataOutputStream(socket.getOutputStream())) {
+                int command = input.readInt();
+                switch (command) {
+                    case 2 -> {
+                        int nextID = getNextID();
+                        int nameLength = input.readInt();
+                        byte[] nameBytes = new byte[nameLength];
+                        //noinspection ResultOfMethodCallIgnored
+                        input.read(nameBytes, 0, nameLength);
+                        String name = new String(nameBytes);
+                        if (name.isEmpty()) {
+                            name = "file" + nextID + ".txt";
                         }
-                        case "GET" -> {
-                            String name = msg[1];
-                            Path path = Paths.get(partOfPath + name);
-                            try {
-                                String content = Files.readString(path);
-                                output.writeUTF("200 " + content);
-                            } catch (IOException e) {
-                                output.writeUTF("404");
-                            }
-                        }
-                        case "DELETE" -> {
-                            String name = msg[1];
-                            Path path = Paths.get(partOfPath + name);
-                            if (Files.deleteIfExists(path)) {
-                                output.writeUTF("200");
-                            } else {
-                                output.writeUTF("404");
-                            }
-                        }
-                        case "exit" -> {
-                            return;
+                        int dataLength = input.readInt();
+                        byte[] data = new byte[dataLength];
+                        //noinspection ResultOfMethodCallIgnored
+                        input.read(data, 0, dataLength);
+                        try {
+                            Files.write(Paths.get(partOfPath + name), data, CREATE_NEW, WRITE);
+                            map.put(nextID, name);
+                            output.writeInt(200);
+                            output.writeInt(nextID);
+                        } catch (IOException e) {
+                            output.writeInt(403);
                         }
                     }
+                    case 1 -> {
+                        int type = input.readInt();
+                        String name = null;
+                        switch (type) {
+                            case 1 -> name = input.readUTF();
+                            case 2 -> {
+                                int id = input.readInt();
+                                name = map.get(id);
+                            }
+                        }
+                        try {
+                            byte[] message = Files.readAllBytes(Paths.get(partOfPath + name));
+                            output.writeInt(200);
+                            output.writeInt(message.length);
+                            output.write(message);
+                        } catch (IOException e) {
+                            output.writeInt(404);
+                        }
+                    }
+                    case 3 -> {
+                        int type = input.readInt();
+                        String name = null;
+                        switch (type) {
+                            case 1 -> name = input.readUTF();
+                            case 2 -> {
+                                int id = input.readInt();
+                                name = map.get(id);
+                            }
+                        }
+                        if (Files.deleteIfExists(Paths.get(partOfPath + name))) {
+                            output.writeInt(200);
+                        } else {
+                            output.writeInt(404);
+                        }
+                    }
+                    case 0 -> {
+                        output.writeInt(200);
+                        stopServer();
+                    }
                 }
+                socket.close();
+            } catch (IOException ignored) {
             }
         }
     }
